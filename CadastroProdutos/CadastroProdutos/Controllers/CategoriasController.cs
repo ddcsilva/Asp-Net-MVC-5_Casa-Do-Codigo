5abd9883957b94b00f005e379cf4167e68d96f88
Danilo Silva <danilo.silva@msn.com>
2021-03-07 19:39:10 -0300

    Criação de Projeto + Controller, Model e View da Categoria

---
+++ CadastroProdutos/CadastroProdutos/Controllers/CategoriasController.cs
@@ -0,0 +1,23 @@
+﻿using CadastroProdutos.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace CadastroProdutos.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+            {
+                new Categoria() { CategoriaId = 1, Nome = "Notebooks" },
+                new Categoria() { CategoriaId = 2, Nome = "Monitores" },
+                new Categoria() { CategoriaId = 3, Nome = "Impressoras" },
+                new Categoria() { CategoriaId = 4, Nome = "Mouses" },
+                new Categoria() { CategoriaId = 5, Nome = "Desktops" }
+            };
+
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+    }
+}
---
+++ CadastroProdutos/CadastroProdutos/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace CadastroProdutos.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
